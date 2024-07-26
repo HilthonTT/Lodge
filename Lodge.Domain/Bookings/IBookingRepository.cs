92f5b97407c48480af623c866248bdf3195bd804
Timothy Tandt <Timothy.tandt@outlook.be>
2024-07-26 14:32:18 +0200

    Added the booking & review repositories

---
+++ Lodge.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,36 @@
+﻿using Lodge.Domain.Apartements;
+using Lodge.Domain.Core.Primitives.Maybe;
+
+namespace Lodge.Domain.Bookings;
+
+/// <summary>
+/// Represents the booking repository interface
+/// </summary>
+public interface IBookingRepository
+{
+    /// <summary>
+    /// Gets the booking with the specified identifier.
+    /// </summary>
+    /// <param name="id">The booking identifier.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The maybe instance that may contain the booking with the specified identifier.</returns>
+    Task<Maybe<Booking>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Checks if a booking is overlapping with the specified apartment and duration.
+    /// </summary>
+    /// <param name="apartment">The apartment to check for overlapping bookings.</param>
+    /// <param name="duration">The duration of the booking.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task representing the asynchronous operation that returns true if there is an overlap, otherwise false.</returns>
+    Task<bool> IsOverlappingAsync(
+        Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Adds the specified booking to the database.
+    /// </summary>
+    /// <param name="booking">The booking to be added to the database.</param>
+    void Add(Booking booking);
+}
---
+++ Lodge.Domain/Reviews/IReviewRepository.cs
@@ -0,0 +1,38 @@
+﻿using Lodge.Domain.Core.Primitives.Maybe;
+
+namespace Lodge.Domain.Reviews;
+
+/// <summary>
+/// Represents the review repository interface
+/// </summary>
+public interface IReviewRepository
+{
+    /// <summary>
+    /// Gets the review with the specified identifier.
+    /// </summary>
+    /// <param name="id">The review identifier.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The maybe instance that may contain the review with the specified identifier.</returns>
+    Task<Maybe<Review>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Checks if a user has already reviewed a specified apartment.
+    /// </summary>
+    /// <param name="apartmentId">The apartment identifier.</param>
+    /// <param name="userId">The user identifier.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task representing the asynchronous operation that returns true if the user has already reviewed the apartment, otherwise false.</returns>
+    Task<bool> HasAlreadyReviewed(Guid apartmentId, Guid userId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Inserts the specified review into the database.
+    /// </summary>
+    /// <param name="review">The review to be inserted into the database.</param>
+    void Insert(Review review);
+
+    /// <summary>
+    /// Removes the specified review from the database.
+    /// </summary>
+    /// <param name="review">The review to be removed from the database.</param>
+    void Remove(Review review);
+}
