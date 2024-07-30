0e8ebe8673fa80865efb00b30ca24d5f2266c087
Timothy Tandt <Timothy.tandt@outlook.be>
2024-07-30 14:38:46 +0200

    Folder restructuration & Architectural tests
    - Restructured the file system.
    - Domain layer test
    - Layer tests

---
+++ src/backend/ArchitectureTests/BaseTest.cs
@@ -0,0 +1,17 @@
+﻿using Lodge.Application.Abstractions.Messaging;
+using Lodge.Domain.Users;
+using Lodge.Infrastructure.Caching;
+using Lodge.Persistence;
+using Lodge.Presentation.Endpoints;
+using System.Reflection;
+
+namespace ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(CacheService).Assembly;
+    protected static readonly Assembly PersistenceAssembly = typeof(LodgeDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(IEndpoint).Assembly;
+}
---
+++ src/backend/ArchitectureTests/Layers/LayerTests.cs
@@ -0,0 +1,84 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace ArchitectureTests.Layers;
+
+public class LayerTests : BaseTest
+{
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOn_ApplicationLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOn_PersistenceLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(PersistenceAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_Should_NotHaveDependencyOn_InfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_Should_NotHaveDependencyOn_PersistenceLayer()
+    {
+        TestResult result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(PersistenceAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void InfrastructureLayer_Should_NotHaveDependencyOn_PresentationLayer()
+    {
+        TestResult result = Types.InAssembly(InfrastructureAssembly)
+           .Should()
+           .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+           .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void PersistenceLayer_Should_NotHaveDependencyOn_PresentationLayer()
+    {
+        TestResult result = Types.InAssembly(PersistenceAssembly)
+           .Should()
+           .NotHaveDependencyOn(PresentationAssembly.GetName().Name)
+           .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
