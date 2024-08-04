93284559ab3b72420090a7d327c186958514b956
Timothy Tandt <Timothy.tandt@outlook.be>
2024-08-04 18:11:16 +0200

    Added YARP Balancing.

---
+++ src/backend/Yarp.Balancer/Program.cs
@@ -0,0 +1,41 @@
+using System.Threading.RateLimiting;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("fixed-by-ip", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString(),
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 50,
+                Window = TimeSpan.FromMinutes(1)
+            }));
+});
+
+builder.Services.AddHealthChecks();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+app.UseRateLimiter();
+
+app.MapHealthChecks("health");
+
+app.Run();
