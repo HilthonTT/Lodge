90c7e21300f9d44c831ad99a70dbd59c3fc6efa5
Timothy Tandt <Timothy.tandt@outlook.be>
2024-08-04 15:11:56 +0200

    Added ShadCN-UI

--- src/frontend/app/(auth)/login/page.tsx
+++ src/frontend/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { TentTree } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 import { login, storeToken } from "@/lib/auth";
@@ -30,7 +31,13 @@ const LoginPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-center text-gray-900">Login</h1>
+        <div className="flex flex-col items-center justify-center">
+          <TentTree className="size-16 text-center text-indigo-500" />
+          <h1 className="text-2xl font-bold text-center text-gray-900 capitalize">
+            Login to your account
+          </h1>
+        </div>
+
         <form onSubmit={handleLogin} className="space-y-6">
           <div>
             <label
